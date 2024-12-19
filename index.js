2bc97536d986a035f540ba7435afb235c53f2235
Khazeral <wandehousse@gmail.com>
2024-12-19 23:25:34 +0100

    feat : implement Leventstein algorythm

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 import {films} from "./filmList.js";
-import {getRandomInt} from "./utils.js";
+import {getLevenshteinDistance, getRandomInt} from "./utils.js";
 
 let movieSelected = ""
 
+const moviesToGuess = films
+
 const getRandomMovie = () => {
   const pTitle = document.getElementById("film-title");
-
   if (pTitle) {
     const randomIndex = getRandomInt(films.length);
     movieSelected = films[randomIndex]
     pTitle.textContent = films[randomIndex].emoji;
   }
 };
+
+const validateButton = document.getElementById("validate-button")
+const input = document.getElementById("input-answer")
+
+const checkAnswer = () => {
+   const distance = getLevenshteinDistance(input.value, movieSelected.name)
+
+   if(distance > 3 ){
+    console.log("non")
+   }
+}
+validateButton.addEventListener(
+  "click",
+  () => {
+    checkAnswer
+  }
+);
 
 getRandomMovie()
--- utils.js
+++ utils.js
@@ -1,5 +1,50 @@
-const getRandomInt = (max) => {
+export const getRandomInt = (max) => {
   return Math.floor(Math.random() * max);
 };
 
-export { getRandomInt };
+
+const initiateMatrix = (firstWord, secondWord) => {
+  const rows = firstWord.length;
+  const cols = secondWord.length;
+  const matrix = [];
+
+  for (let i = 0; i <= rows; i++) {
+    matrix[i] = new Array(cols + 1).fill(0);
+
+    if (i === 0) {
+      for (let j = 0; j <= cols; j++) {
+        matrix[i][j] = j;
+      }
+    } else {
+      matrix[i][0] = i;
+    }
+  }
+
+  return matrix;
+};
+
+const fillMatrix = (matrix, firstWord, secondWord) => {
+  const str1 = firstWord;
+  const str2 = secondWord;
+
+  for (let i = 1; i <= str1.length; i++) {
+    for (let j = 1; j <= str2.length; j++) {
+      const cost = str1[i - 1] === str2[j - 1] ? 0 : 1;
+
+      matrix[i][j] = Math.min(
+        matrix[i][j - 1] + 1,
+        matrix[i - 1][j] + 1,
+        matrix[i - 1][j - 1] + cost 
+      );
+    }
+  }
+
+  return matrix;
+};
+
+export const getLevenshteinDistance = (firstWord, secondWord) => {
+  const matrix = initiateMatrix(firstWord, secondWord);
+  const filledMatrix = fillMatrix(matrix, firstWord, secondWord);
+  return filledMatrix[firstWord.length][secondWord.length];
+};
+
