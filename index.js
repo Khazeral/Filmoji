07f7b8c6d560934bad82c11aa71e7f6c13f566bb
Khazeral <wandehousse@gmail.com>
2024-12-21 23:23:16 +0100

    feat : add scores

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ import { getLevenshteinDistance, getRandomInt } from "./utils.js";
 
 const errorMessage = document.getElementById("error-message");
 const scorePanel = document.getElementById('score')
-
+let playerScore = 0;
+scorePanel.textContent = playerScore
 
 let movieSelected = "";
-
-const moviesToGuess = films;
 
 const getRandomMovie = () => {
   const pTitle = document.getElementById("film-title");
@@ -30,10 +29,12 @@ const checkAnswer = () => {
   const distance = getLevenshteinDistance(input.value, movieSelected.name);
 
   if (distance > 3) {
+    console.log("pas bon")
     errorMessage.textContent = "C'est pas bon";
     errorMessage.classList.remove("hidden");
     return false;
   }
+  scorePanel.textContent = playerScore+1
   return true;
 };
 
