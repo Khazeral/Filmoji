9ce093cd26358076b2fda22dcc7eb151c257ebc4
Khazeral <wandehousse@gmail.com>
2024-12-21 23:04:31 +0100

    feat : add some styles

--- index.js
+++ index.js
@@ -1,34 +1,55 @@
-import {films} from "./filmList.js";
-import {getLevenshteinDistance, getRandomInt} from "./utils.js";
+import { films } from "./filmList.js";
+import { getLevenshteinDistance, getRandomInt } from "./utils.js";
 
-let movieSelected = ""
+const errorMessage = document.getElementById("error-message");
+const scorePanel = document.getElementById('score')
 
-const moviesToGuess = films
+
+let movieSelected = "";
+
+const moviesToGuess = films;
 
 const getRandomMovie = () => {
   const pTitle = document.getElementById("film-title");
   if (pTitle) {
     const randomIndex = getRandomInt(films.length);
-    movieSelected = films[randomIndex]
+    movieSelected = films[randomIndex];
     pTitle.textContent = films[randomIndex].emoji;
   }
 };
 
-const validateButton = document.getElementById("validate-button")
-const input = document.getElementById("input-answer")
+const validateButton = document.getElementById("validate-button");
+const input = document.getElementById("input-answer");
+
+const resetErrorMessage = () => {
+  errorMessage.textContent = "";
+  errorMessage.classList.add("hidden");
+};
 
 const checkAnswer = () => {
-   const distance = getLevenshteinDistance(input.value, movieSelected.name)
+  const distance = getLevenshteinDistance(input.value, movieSelected.name);
 
-   if(distance > 3 ){
-    console.log("non")
-   }
-}
-validateButton.addEventListener(
-  "click",
-  () => {
-    checkAnswer
+  if (distance > 3) {
+    errorMessage.textContent = "C'est pas bon";
+    errorMessage.classList.remove("hidden");
+    return false;
   }
-);
+  return true;
+};
 
-getRandomMovie()
+const changeMovie = () => {
+  const isGoodAnswer = checkAnswer();
+  if (isGoodAnswer) {
+    getRandomMovie();
+  }
+};
+
+input.addEventListener("click", () => {
+  resetErrorMessage();
+});
+
+validateButton.addEventListener("click", () => {
+  changeMovie();
+});
+
+getRandomMovie();
